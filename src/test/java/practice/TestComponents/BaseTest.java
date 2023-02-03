89a1b566fc911ab7228d6ad5fe32fb5182bd8bef
daus2936 <55316038+daus2936@users.noreply.github.com>
2023-02-03 16:22:42 +0700

    Update BaseTest.java

--- src/test/java/practice/TestComponents/BaseTest.java
+++ src/test/java/practice/TestComponents/BaseTest.java
@@ -38,7 +38,7 @@ public class BaseTest {
 	public WebDriver initDriver() throws IOException {
 		//properties class
 		Properties prop = new Properties();
-		FileInputStream fis = new FileInputStream(System.getProperty("user.dir")+"\\src\\main\\java\\practice\\resources\\GlobalData.properties");
+		FileInputStream fis = new FileInputStream(System.getProperty("user.dir")+"/src/main/java/practice/resources/GlobalData.properties");
 		prop.load(fis);
 		
 		String browserName = System.getProperty("browser")!=null ? System.getProperty("browser") : prop.getProperty("browser");
